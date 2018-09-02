c96c17219660aacd5837526cbc5a2fa72f015236
Tech-Banerg <39728935+Techbanerg@users.noreply.github.com>
2018-09-02 18:02:05 +0530

    Add files via upload

---
+++ Decorators.py
@@ -0,0 +1,32 @@
+import time
+
+
+def timing_function(some_function):
+
+    """
+    Outputs the time a function takes
+    to execute.
+    """
+
+    def wrapper():
+        t1 = time.time()
+        print('DuurBara')
+        some_function()
+        t2 = time.time()
+        print('laora bara')
+        return "Time it took to run the function: " + str((t2 - t1)) + "\n"
+    return wrapper
+
+
+@timing_function
+def my_function():
+    num_list = []
+    for num in (range(0, 10000)):
+        num_list.append(num)
+    print("\nSum of all the numbers: " + str((sum(num_list))))
+
+if __name__ == "__main__":
+   print("Value of __name__is:", __name__)
+   print(my_function())
+   #print(dir(__builtins__))
+   
---
+++ while_loop.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+count = 0
+while (count < 9):
+   print ('The count is:', count)
+   count = count + 1
+
+print('GoodBye!')
